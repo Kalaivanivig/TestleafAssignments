7c4f718ba5cc53cca83848aef634a619d6f3f993
Kalaivanivig <110737429+Kalaivanivig@users.noreply.github.com>
2022-08-08 21:09:01 -0400

    Add files via upload

---
+++ Myassignments/src/main/java/week1/day2/PrintOddNumbers.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class PrintOddNumbers {
+
+	public static void main(String[] args) {
+		for (int i = 31; i <= 66; i++) {
+			if (i % 2 != 0) {
+				System.out.println("ODD NUMBER: " + i);
+			}
+		}
+
+	}
+
+}
